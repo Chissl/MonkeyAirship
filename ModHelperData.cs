ce41b890c507b7253da4df16818064e66275f869
Commander-Cat101 <102434202+Commander-Cat101@users.noreply.github.com>
2023-03-18 12:39:04 +1300

    Add files via upload

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,7 +2,8 @@ namespace MonkeyAirship
 {
     public static class ModHelperData
     {
-        public const string Version = "1.0.0";
+        public const string Version = "1.1.0";
+        public const string WorksOnVersion = "35";
         public const string Name = "MonkeyAirship";
 
         public const string Description = "This mods adds the Monkey Airship, a powerful flying tower that obliterates bloons!";
